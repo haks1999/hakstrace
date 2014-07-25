d913eb32f5b6296ae55c4455a377167125c6044f
haks1999 <haks1999@gmail.com>
2014-07-25 14:38:08 +0900

    test

--- src/main/java/sample/data/jpa/web/SampleController.java
+++ src/main/java/sample/data/jpa/web/SampleController.java
@@ -30,7 +30,7 @@ public class SampleController {
 	@Autowired
 	private CityService cityService;
 
-	@RequestMapping("/")
+	@RequestMapping("/city")
 	@ResponseBody
 	@Transactional(readOnly = true)
 	public String helloWorld() {
