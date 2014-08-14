397d861940bde65a0dc7451f7642a45f844c2006
haks1999 <haks1999@gmail.com>
2014-08-14 17:34:07 +0900

    gogo

--- src/main/java/com/hakstrace/system/user/controller/UserController.java
+++ src/main/java/com/hakstrace/system/user/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.hakstrace.system.user.controller;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,16 +18,21 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping("/system/users")
-	public String forwardToUserList(Map<String, Object> model) {
+	@RequestMapping(value="/system/users", method = RequestMethod.GET, consumes="!application/json")
+	public String forwardToUserList() {
 		return "hakstrace/system/user/user_list";
 	}
 	
-	@RequestMapping("/system/users/list")
+	@RequestMapping(value="/system/users", method = RequestMethod.GET, consumes="application/json")
 	@ResponseBody
 	public Page<User> retrieveUserList(
 										@RequestParam(value = "userName", required = false, defaultValue="") String userName,
 										@RequestParam(value = "pageNum", required = false, defaultValue="1") int pageNum) {
 		return userService.findAll(pageNum, userName);
+	}
+	
+	@RequestMapping(value="/system/users/{userId}", method = RequestMethod.GET, consumes="!application/json")
+	public String forwardToUserDetail(@PathVariable String userId) {
+		return "hakstrace/system/user/user_detail";
 	}
 }
--- src/main/resources/static/js/hakstrace.js
+++ src/main/resources/static/js/hakstrace.js
@@ -31,56 +31,80 @@ hakstrace.loadPageableTable = function(tableId, url, param, callbacks){
 	$(targetTable).find('tbody tr').not('.hide').remove();
 	$(targetTable).find("tfoot div.pagination li").removeClass("disabled");
 	param.pageNum = param.pageNum || $(targetTable).attr("pagenum");
-	$.getJSON(url,param).done(function(pageableList){
-		var dataList = pageableList.content;
-		for( var inx=0; inx < dataList.length; inx++ ){
-			var copiedRow = $(targetTable).find('tbody tr.hide').clone();
-			if( callbacks && callbacks.row ){
-				callbacks.row.call(null, copiedRow);	
-			}else{
-				$(copiedRow).find('td').each(function(){
-					var colname=$(this).attr("abbr");
-					if( callbacks && callbacks.cols && callbacks.cols[colname] ){
-						$(this).html(callbacks.cols[colname].call(null, dataList[inx]));
-					}else{
-						if(colname && colname.length>0){
-							var colnameArr = colname.split(".");
-							var colnameParent = dataList[inx];
-							for( var jnx=0;jnx<colnameArr.length;jnx++){
-								colnameParent = colnameParent[colnameArr[jnx]];
+	
+	
+	contentType: 'application/json'
+		
+	$.ajax({
+		contentType: 'application/json',
+		url: url,
+		type: 'GET',
+		data: param,
+		success: function(pageableList) {
+			var dataList = pageableList.content;
+			for( var inx=0; inx < dataList.length; inx++ ){
+				var copiedRow = $(targetTable).find('tbody tr.hide').clone();
+				if( callbacks && callbacks.row ){
+					callbacks.row.call(null, copiedRow);	
+				}else{
+					$(copiedRow).find('td').each(function(){
+						var colname=$(this).attr("abbr");
+						if( callbacks && callbacks.cols && callbacks.cols[colname] ){
+							$(this).html(callbacks.cols[colname].call(null, dataList[inx]));
+						}else{
+							if(colname && colname.length>0){
+								var colnameArr = colname.split(".");
+								var colnameParent = dataList[inx];
+								for( var jnx=0;jnx<colnameArr.length;jnx++){
+									colnameParent = colnameParent[colnameArr[jnx]];
+								}
+								$(this).text(colnameParent);
 							}
-							$(this).text(colnameParent);
 						}
-					}
+					});
+				}
+				$(copiedRow).removeClass('hide');
+				$(targetTable).append(copiedRow);
+			}
+			$(targetTable).find("tfoot div.dataTables_info").text("Showing " + dataList.length + " to " + pageableList.numberOfElements + " of " + pageableList.totalElements + " entries");
+			$(targetTable).find("tfoot ul.pagination li a").unbind();
+			if(pageableList.firstPage == true){
+				$(targetTable).find("tfoot ul.pagination li.prev").addClass("disabled");
+			}else{
+				$(targetTable).find("tfoot ul.pagination li.prev a").click(function(){
+					var prevParam = param;
+					prevParam.pageNum = Number(pageableList.number);
+					hakstrace.loadPageableTable(tableId, url, prevParam);
 				});
 			}
-			$(copiedRow).removeClass('hide');
-			$(targetTable).append(copiedRow);
-		}
-		$(targetTable).find("tfoot div.dataTables_info").text("Showing " + dataList.length + " to " + pageableList.numberOfElements + " of " + pageableList.totalElements + " entries");
-		$(targetTable).find("tfoot ul.pagination li a").unbind();
-		if(pageableList.firstPage == true){
-			$(targetTable).find("tfoot ul.pagination li.prev").addClass("disabled");
-		}else{
-			$(targetTable).find("tfoot ul.pagination li.prev a").click(function(){
-				var prevParam = param;
-				prevParam.pageNum = Number(pageableList.number);
-				hakstrace.loadPageableTable(tableId, url, prevParam);
-			});
-		}
-		if(pageableList.lastPage == true){
-			$(targetTable).find("tfoot ul.pagination li.next").addClass("disabled");
-		}else{
-			$(targetTable).find("tfoot ul.pagination li.next a").click(function(){
-				var nextParam = param;
-				nextParam.pageNum = Number(pageableList.number)+2;
-				hakstrace.loadPageableTable(tableId, url, nextParam);
-			});
-		}
-		
-		
-		if( callbacks && callbacks.after ){
-			callbacks.after.call(null);	
-		}
+			if(pageableList.lastPage == true){
+				$(targetTable).find("tfoot ul.pagination li.next").addClass("disabled");
+			}else{
+				$(targetTable).find("tfoot ul.pagination li.next a").click(function(){
+					var nextParam = param;
+					nextParam.pageNum = Number(pageableList.number)+2;
+					hakstrace.loadPageableTable(tableId, url, nextParam);
+				});
+			}
+			
+			
+			if( callbacks && callbacks.after ){
+				callbacks.after.call(null);	
+			}
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			alert(errorThrown);
+		},
+		dataType: "json",
+		async: false
 	});
+		
+		
+	
+};
+
+
+hakstrace.loadContent = function(url){
+	window.location.hash = url;
+	LoadAjaxContent(url);	//devoops.js
 };
